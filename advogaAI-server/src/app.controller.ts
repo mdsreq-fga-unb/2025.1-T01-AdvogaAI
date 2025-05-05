a02b52c4b8851a06a8ea695aee8ae2e367f92f25
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <anapfeilsticker@gmail.com>
2025-05-05 09:47:59 -0300

    docs: add in app controller routes the ApiOperation tag

--- advogaAI-server/src/app.controller.ts
+++ advogaAI-server/src/app.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-// @ApiTags('app')
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @ApiOperation({ summary: 'Get Hello World' })
-  // @ApiResponse({ status: 200, description: 'Hello World!' })
+  @ApiOperation({ summary: 'Get Hello World' })
+  @ApiResponse({ status: 200, description: 'Hello World!' })
   getHello(): string {
     return this.appService.getHello();
   }
