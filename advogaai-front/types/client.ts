66b40314b89c81a72dc8edecbd237caf6599d0df
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <anapfeilsticker@gmail.com>
2025-06-23 18:44:01 -0300

    feat: integrate back and frontend from client page

---
+++ advogaai-front/types/client.ts
@@ -0,0 +1,47 @@
+import { CreateAddressDto } from './address';
+
+export enum EstadoCivil {
+  SOLTEIRO = 'SOLTEIRO',
+  CASADO = 'CASADO',
+  DIVORCIADO = 'DIVORCIADO',
+  VIUVO = 'VIUVO',
+  UNIAO_ESTAVEL = 'UNIAO_ESTAVEL',
+}
+export interface CreatePessoaFisicaDto {
+  nomeCompleto: string;
+  cpf: string;
+  rg: string;
+  ctps: string;
+  nacionalidade: string;
+  email: string;
+  telefone: string;
+  estadoCivil: EstadoCivil;
+  profissao: string;
+  endereco: CreateAddressDto;
+}
+export interface RegisterJuridicalClientDto {
+  razaoSocial: string;
+  nomeFantasia: string;
+  cnpj: string;
+  inscricaoEstadual: string;
+  email: string;
+  telefone: string;
+  endereco: CreateAddressDto;
+  representante: CreatePessoaFisicaDto;
+}
+
+export interface PessoaFisica {
+  id: string;
+  nomeCompleto: string;
+  cpf: string;
+  rg: string;
+  ctps: string;
+  nacionalidade: string;
+  email: string;
+  telefone: string;
+  estadoCivil: EstadoCivil;
+  profissao: string;
+  endereco: CreateAddressDto;
+  createdAt: string;
+  updatedAt: string;
+}
