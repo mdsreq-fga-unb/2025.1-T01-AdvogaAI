7472117f548cfd30efb4187adda005547c8221f1
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <anapfeilsticker@gmail.com>
2025-06-28 15:56:08 -0300

    fix: fix module bug

--- advogaAI-server/src/storage/storage.module.ts
+++ advogaAI-server/src/storage/storage.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
+import { ConfigModule } from '@nestjs/config';
+import storageConfig from './storage.config';
+import { S3Provider } from './s3.provider';
 
 @Module({
-  providers: [StorageService],
+  providers: [StorageService, S3Provider],
+  imports: [ConfigModule.forFeature(storageConfig)],
 })
 export class StorageModule {}
