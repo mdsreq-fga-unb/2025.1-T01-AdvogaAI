918ebf6a32e0040ce2f36ca6e7038ebaee7e5e4f
yanzin00 <ylsg0303@gmail.com>
2025-06-28 16:49:02 -0300

    feat: controller delete document

--- advogaAI-server/src/document-models/document-models.controller.ts
+++ advogaAI-server/src/document-models/document-models.controller.ts
@@ -1,4 +1,20 @@
 import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  //UseGuards,
+} from '@nestjs/common';
+//import { JwtAuthGuard } from 'src/shared/jwt/jwt-auth.guard';
 
 @Controller('document-models')
-export class DocumentModelsController {}
+export class DocumentModelsController {
+  //@UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  deletePessoaJuridica(@Param('id') id: string) {
+    return id;
+  }
+}
